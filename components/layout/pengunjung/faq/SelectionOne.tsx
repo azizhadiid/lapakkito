4b9a4ad8a82b4285c68879700c586fd089d2f957
ZikraZana <monozikk13@gmail.com>
2025-10-30 12:10:28 +0700

    Color Text FAQ Page (Progress 3)

--- components/layout/pengunjung/faq/SelectionOne.tsx
+++ components/layout/pengunjung/faq/SelectionOne.tsx
@@ -2,8 +2,8 @@ export default function SectionOne({ faqs }: { faqs: { q: string; a: string }[] 
     return (
         <section className="relative py-23 md:py-30">
             <div className="max-w-xl md:max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-                <h1 className="text-2xl md:text-3xl text-center font-semibold mb-2">Frequently Asked Questions</h1>
-                <p className="text-gray-600 mb-6 text-center text-sm md:text-base">Pertanyaan yang sering diajukan tentang penggunaan LapakKito.</p>
+                <h1 className="text-2xl md:text-3xl text-center font-semibold text-[#4E4039] mb-2">Frequently Asked Questions</h1>
+                <p className=" mb-6 text-center text-sm md:text-base text-[#4E4039]">Pertanyaan yang sering diajukan tentang penggunaan LapakKito.</p>
                 
                 {/* Desktop */}
                 <div className="hidden md:grid grid-cols-2 gap-x-3">
@@ -22,10 +22,10 @@ export default function SectionOne({ faqs }: { faqs: { q: string; a: string }[] 
                                             className="group bg-white border border-gray-200 rounded-lg p-4 shadow-lg hover:shadow-xl transition-shadow duration-200"
                                         >
                                             <summary className="cursor-pointer flex justify-between items-center list-none min-h-10">
-                                                <span className="font-medium text-gray-800">{faq.q}</span>
-                                                <span className="ml-3 text-gray-500 transition-transform group-open:rotate-180">▾</span>
+                                                <span className="font-medium text-[#4E4039]">{faq.q}</span>
+                                                <span className="ml-3 text-[#4E4039] transition-transform group-open:rotate-180">▾</span>
                                             </summary>
-                                            <div className="mt-3 text-gray-700">{faq.a}</div>
+                                            <div className="mt-3 text-[#4E4039]">{faq.a}</div>
                                         </details>
                                     ))}
                                 </div>
@@ -38,10 +38,10 @@ export default function SectionOne({ faqs }: { faqs: { q: string; a: string }[] 
                                             className="group bg-white border border-gray-200 rounded-lg p-4 shadow-lg hover:shadow-xl transition-shadow duration-200"
                                         >
                                             <summary className="cursor-pointer flex justify-between items-center list-none min-h-10">
-                                                <span className="font-medium text-gray-800">{faq.q}</span>
-                                                <span className="ml-3 text-gray-500 transition-transform group-open:rotate-180">▾</span>
+                                                <span className="font-medium text-[#4E4039]">{faq.q}</span>
+                                                <span className="ml-3 text-[#4E4039] transition-transform group-open:rotate-180">▾</span>
                                             </summary>
-                                            <div className="mt-3 text-gray-700">{faq.a}</div>
+                                            <div className="mt-3 text-[#4E4039]">{faq.a}</div>
                                         </details>
                                     ))}
                                 </div>
@@ -58,10 +58,10 @@ export default function SectionOne({ faqs }: { faqs: { q: string; a: string }[] 
                             className="group bg-white border border-gray-200 rounded-lg p-4 shadow-lg hover:shadow-xl transition-shadow duration-200"
                         >
                             <summary className="cursor-pointer flex justify-between items-center list-none">
-                                <span className="font-medium text-gray-800">{faq.q}</span>
-                                <span className="ml-3 text-gray-500 transition-transform group-open:rotate-180">▾</span>
+                                <span className="font-medium text-[#4E4039]">{faq.q}</span>
+                                <span className="ml-3 text-[#4E4039] transition-transform group-open:rotate-180">▾</span>
                             </summary>
-                            <div className="mt-3 text-gray-700">{faq.a}</div>
+                            <div className="mt-3 text-[#4E4039]">{faq.a}</div>
                         </details>
                     ))}
                 </div>
