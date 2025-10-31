32047ef4b471b874571218f3a874b2d9f7773cb1
Arfun Ali Yafie <alinews120@gmail.com>
2025-10-31 11:52:19 +0700

    optional

--- components/layout/admin/pages/beranda.tsx
+++ components/layout/admin/pages/beranda.tsx
@@ -8,14 +8,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Navbar } from "../../pengunjung/Navbar";
+import { MainLayoutPengungjung } from "../../pengunjung/MainLayoutPengunjung";
+import {footer} from '../../pengunjung/Footer';
 
-export default function BerandaAdmin() {
+export default function BerandaAdmin({children}: {children: React.ReactNode}) {
   return (
-    <main>
-      <Navbar/>
-      <Card>
-        <CardTitle></CardTitle>
-      </Card>
-    </main>
+    <MainLayoutPengungjung>
+      {children}
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe dignissimos eius earum ea voluptas quod ducimus natus praesentium nesciunt nihil eaque, tempora fugit sunt incidunt repellat quia. Reiciendis, velit exercitationem.</p>
+    </MainLayoutPengungjung>
   );
 }
